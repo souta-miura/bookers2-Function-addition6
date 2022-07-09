dd2c6c6a6ad766cb4b814d8508b5ca137b092687
EC2 Default User <ec2-user@ip-172-31-12-184.ap-northeast-1.compute.internal>
2022-07-09 02:29:00 +0000

    [Finish] 課題５

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about"
-  get "search" => "searches#search_result"
+  
   
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
@@ -18,5 +18,5 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'search' => 'searches#search_result' # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
