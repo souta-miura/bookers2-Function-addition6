d65f3eaedabaef6e052380057625ab4bc4bdac20
EC2 Default User <ec2-user@ip-172-31-12-184.ap-northeast-1.compute.internal>
2022-07-10 01:56:13 +0000

    [Finish] 課題６

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,15 @@ class FavoritesController < ApplicationController
     
     def create
         book = Book.find(params[:book_id])
-        favorite = current_user.favorites.new(book_id: book.id)
-        favorite.save
+        @favorite = current_user.favorites.new(book_id: book.id)
+        @favorite.save
+        render 'replace_fa'
     end
     
     def destroy
         book = Book.find(params[:book_id])
-        favorite = current_user.favorites.find_by(book_id: book.id)
-        favorite.destroy
+        @favorite = current_user.favorites.find_by(book_id: book.id)
+        @favorite.destroy
+        render 'replace_fa'
     end
 end
